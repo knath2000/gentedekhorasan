1012bab738d62154802657b35190435957e52da2
Kalyan Nath <kalyannath@Mac.attlocal.net>
2025-05-17 11:58:04 -0700

    nextjs test2

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  // Redirect to the (tabs) group, which will default to the 'index' screen (HomeScreen)
-  return <Redirect href="/(tabs)" />;
-}
