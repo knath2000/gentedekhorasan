529343ebc71e2827d2fe9478d986193105d2ca95
Kalyan Nath <kalyannath@Mac.attlocal.net>
2025-05-26 13:59:14 -0700

    asdfasdfasdfsadf

--- apps/quran-data-api/api/lib/prisma.ts
+++ apps/quran-data-api/api/lib/prisma.ts
@@ -5,6 +5,9 @@ const adapter = new PrismaNeon({
   connectionString: process.env.NEON_DATABASE_URL
 })
 
+console.log('NEON_DATABASE_URL:', process.env.NEON_DATABASE_URL ? 'Configured' : 'Not Configured');
+console.log('NEON_DATABASE_URL value (first 10 chars):', process.env.NEON_DATABASE_URL?.substring(0, 10));
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
