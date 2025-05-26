3c1eceada8ffb254a4e77d40f4dadcc0541fa9da
Kalyan Nath <kalyannath@Mac.attlocal.net>
2025-05-26 13:52:42 -0700

    asdfasdfasdfsadf

--- apps/quran-data-api/api/lib/prisma.ts
+++ apps/quran-data-api/api/lib/prisma.ts
@@ -1,13 +1,9 @@
 import { PrismaNeon } from '@prisma/adapter-neon';
-import 'websocket-polyfill'; // Añadir polyfill para Edge Functions
 import { PrismaClient } from '../generated/prisma';
 
 const adapter = new PrismaNeon({
   connectionString: process.env.NEON_DATABASE_URL
 })
-
-console.log('NEON_DATABASE_URL:', process.env.NEON_DATABASE_URL ? 'Configured' : 'Not Configured');
-console.log('NEON_DATABASE_URL value (first 10 chars):', process.env.NEON_DATABASE_URL?.substring(0, 10));
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
